69b554cd379359f5e6a6108c2a5fde25a7af2db8
Ericsson <93233951+Ericsson111@users.noreply.github.com>
2023-07-11 22:48:57 -0400

    Update newGameBot_v0.py
    New Update: Threat Level Identification

--- newGameBot_v0.py
+++ newGameBot_v0.py
@@ -81,7 +81,7 @@ def Find_Coordinates_Of_Arrays(PlayerCoordinate: list, ArrayType: str):
         coordinateArray = sorted([list(x) for x in combined_set], key=lambda x: x[0])
     return coordinateArray
 
-def GameBot_Move(Game_Board: list, allArrays: list, playerCoordinate: list):
+def GameBot_Move(Game_Board:list, allArrays: list, playerCoordinate: list):
     arrayNames = ["Horizontal", "Vertical", "PositiveDiagonal", "NegativeDiagonal"]
     # allArrays consists all the arrays that passes through "playerCoordinate"
     # Horizontal, Vertical, Positive/Negative Diagonal
@@ -124,8 +124,12 @@ def GameBot_Move(Game_Board: list, allArrays: list, playerCoordinate: list):
         connectionStreakValueDict[arrayCSVID] = maximumLength
         connectionStreakCoordinateDict[arrayCSVID] = [startingPoint, endingPoint]
 
-    availableArrays = {}
+    print("connectionStreakValueDict:",connectionStreakValueDict)
+    print("connectionStreakCoordinateDict:",connectionStreakCoordinateDict)
 
+    availableArrays = {}
+    availableArraysID = {}
+    availableArraysIssueID = {}
     # Check the arrays that the Player can actually connect 5 pieces
     # Meaning the connection-streak and the enarby empty slots adds up to 5
 
@@ -139,11 +143,6 @@ def GameBot_Move(Game_Board: list, allArrays: list, playerCoordinate: list):
         endingPointInd = ArrayCoordinates[str(currentArray)].index(endingPoint)
 
         playerRemainingPieceToWin = 5 - connectionStreakValueDict[arraySSID] 
-
-        """ 
-        The Player would have between 1 - 3 pieces on an array for most of the time
-        
-        """
         
         print("currentArray:",currentArray)
         print("startingPoint:",startingPoint, "startingPointInd:", startingPointInd)
@@ -151,15 +150,19 @@ def GameBot_Move(Game_Board: list, allArrays: list, playerCoordinate: list):
         print("playerRemainingPieceToWin:",playerRemainingPieceToWin)
 
         # Check in front of the startingPoint of Player's Connection-Streak
-        startScanIssueID = [startScanID for startScanID in range(startingPointInd, 0, -1) if currentArray[startScanID] == 'X']
+        startScanIssueID = [startScanID for startScanID in range(startingPointInd, -1, -1) if currentArray[startScanID] == 'X']
         endScanIssueID = [endScanID for endScanID in range(endingPointInd, len(currentArray), 1) if currentArray[endScanID] == 'X']
         print("startScanIssueID:",startScanIssueID, "endScanIssueID:",endScanIssueID)
 
+
+        """ ######Objective: Check if the array contains enough empty space to build up to 5 or more ######"""
         if len(startScanIssueID) >= 1 and len(endScanIssueID) >= 1:
             # There are Bot pieces both in front and behind the Player's connection-streak
-            print("len(startScanIssueID) >= 1 and len(endScanIssueID) >= 1") 
+            print("len(startScanIssueID) >= 1 and len(endScanIssueID) >= 1")
 
             if endScanIssueID[0] - startScanIssueID[0] > 5:
+                availableArraysIssueID[arraySSID] = [startScanIssueID, endScanIssueID]
+                availableArraysID[arraySSID] = [startingPointInd-1, endingPointInd+1]
                 availableArrays[str(currentArray)] = (endScanIssueID[0] - startScanIssueID[0]) - 1
             else:
                 print("currentArray1:",currentArray, " is not available")
@@ -170,7 +173,8 @@ def GameBot_Move(Game_Board: list, allArrays: list, playerCoordinate: list):
             print("len(startScanIssueID) == 0")
 
             if (endScanIssueID[0] - 1) >= 5:
-                # Enough empty slots to build 5 connections
+                availableArraysIssueID[arraySSID] = [startScanIssueID, endScanIssueID]
+                availableArraysID[arraySSID] = [startingPointInd-1, endingPointInd+1]
                 availableArrays[str(currentArray)] = (endScanIssueID[0] - 1)
             else:
                 print("currentArray2:",currentArray, " is not available")
@@ -181,65 +185,194 @@ def GameBot_Move(Game_Board: list, allArrays: list, playerCoordinate: list):
             print("len(endScanIssueID) == 0")
 
             if 10 - startScanIssueID[0] > 5:
+                availableArraysIssueID[arraySSID] = [startScanIssueID, endScanIssueID]
+                availableArraysID[arraySSID] = [startingPointInd-1, endingPointInd+1]
                 availableArrays[str(currentArray)] = 10 - startScanIssueID[0]
+            else:
+                print("currentArray3:",currentArray, " is not available")
         
         elif len(startScanIssueID) == 0 and len(endScanIssueID) == 0:
             print("# There are completely no Bot pieces at all on the current choosen array")
+            availableArraysIssueID[arraySSID] = [startScanIssueID, endScanIssueID]
+            availableArraysID[arraySSID] = [startingPointInd-1, endingPointInd+1]
             availableArrays[str(currentArray)] = len(currentArray) 
 
         print("-"*50)
 
     print("availableArrays:",availableArrays)
+    print("availableArraysID:",availableArraysID)
+    print("availableArraysIssueID:",availableArraysIssueID)
+
+    if len(availableArrays) > 1: 
+        """
+        The player now has a variety of options to try and line up five pieces in a row.
+        The Bot has to determine the array that poses the most significant threat.
+        The threat could be identified by the Bot pieces next to the Player's connection-streak.
+
+        The player would typically find it challenging and nearly impossible to complete this task 
+        in the case of " XOOO "(case 1) because it only takes one Bot piece to break the streak.
+
+        However if the Bot does nothing about this connection-streak in the instance of "X OOO" (case 2), 
+        the Player would have a 100% win rate. ("X OOOO " <- Next phase of case 2)
+
+        Therefore, it is crucial for the Bot to recognize the various threat levels among the lists 
+        while evaluating which array is most optimized. This will allow the Bot to choose the most 
+        optimal set and execute the manoeuvre that will be most effective.
+        """
+        
+
+        """ ###### Objective: Find the array with Bot pieces on the side of Player's connection-streak ###### """
+ 
+        availableArraysThreatLevel = {}
+        for ArraysThreatScanID in availableArraysID.keys():
+            startIssueID, endIssueID = availableArraysIssueID[ArraysThreatScanID]
+
+            """ ###### Objective: Threat Level Identification ###### """ # Establish Bot decision structure for future use
+            if len(startIssueID) == 0 and len(endIssueID) == 1 or len(startIssueID) == 1 and len(endIssueID) == 0:
+                availableArraysThreatLevel[ArraysThreatScanID] = 1
+            elif len(startIssueID) == 0 and len(endIssueID) == 0:
+                availableArraysThreatLevel[ArraysThreatScanID] = 2
+            elif len(startIssueID) == 1 and len(endIssueID) == 1:
+                availableArraysThreatLevel[ArraysThreatScanID] = 3 
+            """
+            Now there would be three potential scenarios:
+                -> The left side of the connection-streak will have a Bot piece.
+                -> The right side of the connection-streak will have a Bot piece.
+                -> Both side of the connection-streak will have a Bot piece.
+            
+            Threat Level Identification: (O is player and X is Bot)
+                #1 Bot piece is found only on one side of the connection-streak (contain either startIssueID or endIssueID) -> (" OOX " or " OO X ") # Make sure to enclose Player's attempt
+                #2 No Bot piece on the array (empty startIssueID and endIssueID) -> Mostly the line with very limited amount of Player's piece(" O ") # Block one side minimum
+                #3 Bot piece on both side of the connection-streak (contain both startIssueID and endIssueID) -> (" XOOO  X " or "X  OO    x") # Further block Player's attempt
+            """
+        print("availableArraysThreatLevel:",availableArraysThreatLevel)
+
+        PlayerConnectionStreak = [connectionStreakValueDict[availableArraysScanID] for availableArraysScanID in availableArraysID.keys()]
+        equalThreatLevel = list(dict.fromkeys([threatLevel for threatLevel in availableArraysThreatLevel.values()]))
+        print("PlayerConnectionStreak:",PlayerConnectionStreak)
+        print("equalThreatLevel:",equalThreatLevel)
+
+        """
+        Now we must return the "Best Array" that is found to encounter Player's move.
+        According to our "Threat Level Identification", the following code will allow
+        the Bot to fully understand the actions that are required to taken under
+        certain circumstances.
+        """
+
+        selectedArray = None 
+        availableArraysBotVal = {} # array: Number of Bot pieces directly on the side of Player's connection-streak
+        """ Objective: Check if the Bot pieces are directly next to the Player connection-streak ###### """
+        for availableArraysScanID in range(len(availableArrays)):
+            pass
+
+
+'''
+def Finding_Optimized_Bot_Placement(availableArrays: list, connectionStreakCoordinateDict:list, availableArraysCoordinatesID: list, allCoordinates: list): 
+    for availableArraysScanID in range(len(PlayerConnectionStreak)):
+        currentArray = availableArrays[availableArraysScanID]
+        currentArrayThreatLevel = PlayerConnectionStreak[availableArraysScanID]
+        PlayerConnectionStreak = connectionStreakCoordinateDict[availableArraysScanID]
+        startingPointInd, endingPointInd = availableArraysID[availableArraysScanID]
+        startIssueID, endIssueID = availableArraysIssueID[availableArraysScanID]
+        '''
+"""
+            Bot Actions:
+                #1 Determine which connection-streak is the greatest. Then deploy interception Bot piece to prevent Player from winning the game.
+                    -> If all connection-streak are measured with equal length, the (#2 Threat) will be activated and determined to be the threat.
+                        -> No Bot piece on each side possess greater threat than any other under the condition of equal streak length.
+                    -> If (#2 Threat) cannot be activated due to the undetectable Bot piece.
+                        -> (#1 Threat) will be considered as priority rather than (#3 Threat)
+                    -> If (#2 Threat) cannot be activated and only one of the threat level remains while having 2 arrays.
+                        -> If (#1 Threat)
+                            -> Identify the one interception coordinate from each array
+                            -> Use this coordinate to find the other three lines(arrays) other than the current one
+                            -> Find the maximum Player connection-streak length
+                            -> Compare the length on each coordinate and the winner takes on
+                            -> If tied -> rd.randint(0,1)
+                        -> If (#3 Threat)
+                            -> Identify the two interception coordinates from each array
+                            -> Use this coordinates to find the other three lines(arrays) other than the current one
+                            -> Find the maximum Player connection-streak length
+                            -> Compare the length on each coordinate and the winner takes on
+                            -> If tied -> rd.randint(0,1)
+                    -> If (#3 Threat) connection-streak is the greatest, replace with the next level threat unlessthe streak is at 4"""
+'''            # Check if all "Threat Level" are the same 
+            if len(equalThreatLevel) == 1: # Duplication
+                if equalThreatLevel[0] == 1: # All arrays are considered (#1 Threat)
+                    # Find which connection-streak is greater
+                    # If tie, find the other three arrays
+                    pass
+                elif equalThreatLevel[0] == 2: # All arrays are considered (#2 Threat)
+                    # Find which connection-streak is greater
+                    # If tie, find the other three arrays
+                    pass
+                elif equalThreatLevel[0] == 3: # All arrays are considered (#3 Threat)
+                    # Find which connection-streak is greater
+                    # If tie, find the other three arrays
+                    pass
+
+            else: # No duplication
+                # Find the highest connection-streak first
+                pass 
+'''
+            
+def Finding_Optimized_Bot_Placement(availableArrays: list, availableArraysCoordinatesID: list, allCoordinates: list):
+    """
+    availableArrays and allCoordinates are matching by it's index in the list
+
+    Given all the arrays and the coordinates of our target, we must decide which coordinate to go with.
+    First of all, we would need to use "Find_Coordinates_Of_Arrays()" to find the other three arrays for
+    every coordinate, excluding the current given one. Second, we extract these arrays and find the highest
+    Player connection-streak and store it in a dictionary for future comparison. Then, we would extract 
+    these values and compare which ever coordinate have the highest Player connection-streak, and that 
+    coordinate would be returned as Bot's final move.
+    """
+
+    """ ###### Connection-Streak Verification ###### """
+    # How many Player's pieces on a line does not matter, how many are connected matters more
+    connectionStreakValueDict = {}
+    connectionStreakCoordinateDict = {}
+
+    for arrayCSVID in range(len(availableArrays)): # CSV: Connection-Streak Verification
+
+        currentArray = availableArrays[arrayCSVID]
+        currentArrayCoordinates = availableArraysCoordinatesID[arrayCSVID]
+        maximumLength = 0 
+        startingPoint = None 
+        endingPoint = None
+        for charScanID in range(len(currentArray)):
+
+            if currentArray[charScanID] == 'O': # If it's Player's piece
+                phaseLength = 0
+                for playerScanID in range(charScanID, len(currentArray)):
+                    if currentArray[playerScanID] == 'O':
+                        phaseLength += 1
+                    elif currentArray[playerScanID] == 'X' or currentArray[playerScanID] == ' ':
+                        if phaseLength > maximumLength: 
+                            maximumLength = phaseLength
+                            startingPoint = currentArrayCoordinates[charScanID]
+                            endingPoint = currentArrayCoordinates[playerScanID-1]
+                        break
+        
+        connectionStreakValueDict[arrayCSVID] = maximumLength
+        connectionStreakCoordinateDict[arrayCSVID] = [startingPoint, endingPoint]         
+
+        
 
 GameBot_Move(
     Game_Board = [[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], 
-                  [' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X', ' ', ' ', ' '], 
+                  [' ', ' ', 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], 
+                  [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], 
+                  [' ', ' ', ' ', ' ', 'O', 'X', 'O', ' ', ' ', ' ', ' '], 
+                  [' ', ' ', ' ', ' ', 'X', 'O', ' ', ' ', ' ', ' ', ' '], 
+                  [' ', ' ', ' ', ' ', 'X', 'O', ' ', ' ', ' ', ' ', ' '], 
+                  [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], 
                   [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X', ' ', ' '], 
-                  [' ', ' ', ' ', ' ', 'X', ' ', 'X', 'O', ' ', ' ', 'X'], 
-                  [' ', ' ', ' ', ' ', 'X', 'O', 'O', 'O', ' ', ' ', ' '], 
-                  [' ', ' ', ' ', 'X', 'O', 'O', 'O', ' ', ' ', ' ', ' '], 
-                  [' ', ' ', ' ', ' ', ' ', 'X', ' ', ' ', ' ', ' ', ' '], 
-                  [' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X', ' ', ' ', ' '], 
                   [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], 
                   [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], 
                   [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']],
-    allArrays = [[' ', ' ', ' ', ' ', 'X', ' ', 'X', 'O', ' ', ' ', 'X'],
-                 [' ', 'X', ' ', 'O', 'O', ' ', ' ', 'X', ' ', ' ', ' '],
-                 [' ', ' ', 'X', 'O', 'O', 'O', ' ', ' ', ' ', ' ', ' '],
-                 [' ', ' ', ' ', 'O', ' ', ' ', ' ']],
-    playerCoordinate = [3,7])
-
-"""
-Program Output: 
-Allarrays: [[' ', ' ', ' ', ' ', 'X', ' ', 'X', 'O', ' ', ' ', 'X'], [' ', 'X', ' ', 'O', 'O', ' ', ' ', 'X', ' ', ' ', ' '], [' ', ' ', 'X', 'O', 'O', 'O', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', 'O', ' ', ' ', ' ']]
-currentArray: [' ', ' ', ' ', ' ', 'X', ' ', 'X', 'O', ' ', ' ', 'X']
-startingPoint: [3, 7] startingPointInd: 7
-endingPoint: [3, 7] endingPointInd: 7
-playerRemainingPieceToWin: 4
-startScanIssueID: [6, 4] endScanIssueID: [10]
-len(startScanIssueID) >= 1 and len(endScanIssueID) >= 1
-currentArray1: [' ', ' ', ' ', ' ', 'X', ' ', 'X', 'O', ' ', ' ', 'X']  is not available
---------------------------------------------------
-currentArray: [' ', 'X', ' ', 'O', 'O', ' ', ' ', 'X', ' ', ' ', ' ']
-startingPoint: [3, 7] startingPointInd: 3
-endingPoint: [4, 7] endingPointInd: 4
-playerRemainingPieceToWin: 3
-startScanIssueID: [1] endScanIssueID: [7]
-len(startScanIssueID) >= 1 and len(endScanIssueID) >= 1
---------------------------------------------------
-currentArray: [' ', ' ', 'X', 'O', 'O', 'O', ' ', ' ', ' ', ' ', ' ']
-startingPoint: [3, 7] startingPointInd: 3
-endingPoint: [5, 5] endingPointInd: 5
-playerRemainingPieceToWin: 2
-startScanIssueID: [2] endScanIssueID: []
-len(endScanIssueID) == 0
---------------------------------------------------
-currentArray: [' ', ' ', ' ', 'O', ' ', ' ', ' ']
-startingPoint: [3, 7] startingPointInd: 3
-endingPoint: [3, 7] endingPointInd: 3
-playerRemainingPieceToWin: 4
-startScanIssueID: [] endScanIssueID: []
-# There are completely no Bot pieces at all on the current choosen array
---------------------------------------------------
-availableArrays: {"[' ', 'X', ' ', 'O', 'O', ' ', ' ', 'X', ' ', ' ', ' ']": 5, "[' ', ' ', 'X', 'O', 'O', 'O', ' ', ' ', ' ', ' ', ' ']": 8, "[' ', ' ', ' ', 'O', ' ', ' ', ' ']": 7}
-"""
+    allArrays = [[' ', ' ', ' ', ' ', 'O', 'X', 'O', ' ', ' ', ' ', ' '],
+                 [' ', ' ', ' ', 'O', 'X', 'X', ' ', ' ', ' ', ' ', ' '],
+                 [' ', ' ', ' ', 'O', ' ', ' ', ' ', ' '],
+                 [' ', 'X', ' ', 'O', 'O', ' ', ' ', 'X', ' ', ' ']],
+    playerCoordinate = [3,4])
